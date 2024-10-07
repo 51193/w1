545de187e385fc5b6493710d1a54f5f47570a60d
51193 <2878740067@qq.com>
2024-10-07 20:31:34 +0800

    提取IState作为状态机的接口

---
+++ component/state/IState.cs
@@ -0,0 +1,25 @@
+﻿using MyGame.Entity;
+
+namespace MyGame.Component
+{
+    public interface IState
+    {
+        public void Enter(IEntity entity);
+        public void Exit(IEntity entity);
+        public void Update(IEntity entity, double delta);
+        public IState HandleStateTransition(IEntity entity);
+
+        public string SaveState()
+        {
+            return GetType().Name;
+        }
+
+        public void LoadState(string state);
+
+        public void TransitToNewState(IEntity entity, IState newState)
+        {
+            Exit(entity);
+            newState.Enter(entity);
+        }
+    }
+}
---
+++ manager/entity/state/StateManager.cs
@@ -0,0 +1,33 @@
+﻿using MyGame.Component;
+using MyGame.Entity;
+using System;
+using System.Collections.Generic;
+
+namespace MyGame.Manager
+{
+    public class StateManager
+    {
+        private readonly List<IState> _states;
+        private readonly IEntity _entity;
+
+        public StateManager(IEntity entity)
+        {
+            _entity = entity;
+        }
+
+        public void AddState(IState state)
+        {
+            _states.Add(state);
+            state.Enter(_entity);
+        }
+
+        public void Update(double delta)
+        {
+            for (int i = 0; i < _states.Count; i++)
+            {
+                _states[i].Update(_entity, delta);
+                _states[i] = _states[i].HandleStateTransition(_entity);
+            }
+        }
+    }
+}
