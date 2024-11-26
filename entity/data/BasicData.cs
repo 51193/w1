8af45b6a46a82933bf4e123ab0dc0a188924c16f
51193 <2878740067@qq.com>
2024-11-26 13:17:04 +0800

    取消保存BasicData中的IsSavable字段

--- entity/data/BasicData.cs
+++ entity/data/BasicData.cs
@@ -1,8 +1,11 @@
-﻿namespace MyGame.Entity.Data
+﻿using System.Text.Json.Serialization;
+
+namespace MyGame.Entity.Data
 {
     public abstract class BasicData
     {
         public int RefCount { get; set; } = 0;
+        [JsonIgnore]
         public virtual bool IsSavable => true;
     }
 }
